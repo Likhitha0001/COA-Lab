bed3529846c194e77668cf3689b40df059888f02
Likhitha0001 <159116967+Likhitha0001@users.noreply.github.com>
2024-08-27 11:28:17 +0530

    Add files via upload

---
+++ lab task-3(two  8 bit sub).asm
@@ -0,0 +1,46 @@
+org 100h
+
+n1 db 83h
+n2 db 15h
+
+start:
+
+mov al, n1
+sub al, n2
+
+
+mov bl, al
+
+mov ah, al
+and ah, 0F0h
+shr ah,4
+add ah, 30h
+cmp ah, 39h
+jle print_first_digit
+add ah, 7  
+
+print_first_digit:
+mov dl,ah
+mov ah,02h
+int 21h
+
+mov ah, bl
+and ah, 0Fh
+add ah, 30h
+cmp ah, 39h 
+jle print_sec_digit
+add ah,7
+
+print_sec_digit:
+mov dl,ah
+mov ah, 02h
+int 21h
+
+mov ah,4Ch
+int 21h
+
+
+
+
+
+
